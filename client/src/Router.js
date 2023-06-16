fd0795167d5b8a6f95c4072ed9c8eb27751f5871
YooZiiX <jeremy.lucas@mines-ales.org>
2023-06-16 20:42:24 +0200

    Create CreateNote Page & Complete it

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import NotesPage from "./pages/NotesPage";
 import ProfilePage from "./pages/ProfilePage";
-import CreateNote from "./pages/CreateNote";
+import CreateNote from "./pages/CreateNotePage";
 
 export default function Router() {
   return (
