c40772cace926d17927ede71623b1236678c7a42
YooZiiX <jeremy.lucas@mines-ales.org>
2023-06-27 22:13:17 +0200

    fix Router problem

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { registerUser } = require("../controllers/userControllers");
 
-const router = express.router();
+const router = express.Router();
 
 router.route("/").post(registerUser);
 
