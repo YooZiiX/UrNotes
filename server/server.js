52713553e73273ae04fba988332a9c47c65e94d4
YooZiiX <jeremy.lucas@mines-ales.org>
2023-06-28 00:13:43 +0200

    finish Registration API with notFound & errorHandler middleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
@@ -23,6 +24,8 @@ app.get("/note/:id", (req, res) => {
 });
 
 app.use("/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is running on Port ${PORT}`));
