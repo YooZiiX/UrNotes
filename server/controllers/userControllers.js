3123dbe599c0c2e2585d3ab91685635669ab584a
YooZiiX <jeremy.lucas@mines-ales.org>
2023-06-28 00:06:16 +0200

    Add userModel with db user creation, password hashing

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,7 +1,35 @@
-const registerUser = async (req, res) => {
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pict } = req.body;
 
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error Occured!");
+  }
+
   res.json({ name, email });
-};
+});
 
 module.exports = { registerUser };
