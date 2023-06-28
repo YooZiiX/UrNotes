a99dac0178cabf11b8da89b32c540ac9161d3404
YooZiiX <jeremy.lucas@mines-ales.org>
2023-06-28 22:06:23 +0200

    user auth frontend & backend

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
 import Container from "../components/elements/Container";
 
-export default function LoginPage() {
+export default function LoginPage({ history }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const userInfo = localStorage.getItem("userInfo");
+  //   if (userInfo) {
+  //     history.push("/");
+  //   }
+  // }, [history]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      setLoading(true);
+
+      const { data } = await axios.post(
+        "/users/login",
+        {
+          email,
+          password,
+        },
+        config
+      );
+      console.log(data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+    } catch (error) {
+      setError(error.response.data.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="overflow-auto">
       <Header />
@@ -16,7 +57,10 @@ export default function LoginPage() {
           </h1>
         </div>
         <div className="w-full flex justify-center m-6">
-          <form className="grid grid-cols-2 grid-rows-3 gap-x-6 gap-y-4">
+          <form
+            className="grid grid-cols-2 grid-rows-3 gap-x-6 gap-y-4"
+            onSubmit={submitHandler}
+          >
             <div className="col-span-2">
               <label htmlFor="email" className="text-lg font-bold">
                 E-Mail
@@ -25,9 +69,11 @@ export default function LoginPage() {
                 type="text"
                 name="email"
                 id="email"
+                value={email}
                 autoComplete="off"
                 className="focus:border block w-full border border-gray-400 rounded-md p-4"
                 placeholder="john.doe@mail.com"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="col-span-2">
@@ -37,27 +83,21 @@ export default function LoginPage() {
               <input
                 type="password"
                 name="password"
-                id="email"
+                id="password"
+                value={password}
                 autoComplete="off"
                 className="focus:border block w-full border border-gray-400 rounded-md p-4"
                 placeholder="mypassword123"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <div className="mx-auto">
+            <div className="mx-auto col-span-2">
               <input
                 type="button"
                 value="Se connecter"
+                onClick={submitHandler}
                 className="bg-blue-500 hover:bg-blue-700 text-white w-36 h-12 font-primary tracking-widest uppercase cursor-pointer rounded-md animate"
               />
-            </div>
-            <div className="mx-auto">
-              <Link to="/signup">
-                <input
-                  type="button"
-                  value="S'inscrire"
-                  className="bg-danger hover:bg-danger-hover text-white w-36 h-12 font-primary tracking-widest uppercase cursor-pointer rounded-md animate"
-                />
-              </Link>
             </div>
           </form>
         </div>
