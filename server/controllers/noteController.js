dc81076eb7eed5c8dac57c370c905a5e8420f87a
YooZiiX <jeremy.lucas@mines-ales.org>
2023-07-06 22:26:36 +0200

    Create Note backend done

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -2,8 +2,20 @@ const Note = require("../models/noteModel");
 const asyncHandler = require("express-async-handler");
 
 const getNotes = asyncHandler(async (req, res) => {
-  const notes = await Note.find();
+  const notes = await Note.find({ user: req.user._id });
   res.json(notes);
 });
 
-module.exports = { getNotes };
+const createNote = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+  if (!title || !content || !category) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  } else {
+    const note = new Note({ user: req.user._id, title, content, category });
+    const createdNote = await note.save();
+    res.status(201).json(createdNote);
+  }
+});
+
+module.exports = { getNotes, createNote };
--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { getNotes } = require("../controllers/noteController");
+const { getNotes, createNote } = require("../controllers/noteController");
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.route("/").get(protect, getNotes);
-// router.route("/create").post();
+router.route("/create").post(protect, createNote);
 // router.route("/:id").get().put().delete();
 
 module.exports = router;
