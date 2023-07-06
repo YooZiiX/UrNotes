647a7974ebe7386c85535e6f500437796a084406
YooZiiX <jeremy.lucas@mines-ales.org>
2023-07-06 22:58:50 +0200

    deleteNote back-end feature

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -53,7 +53,13 @@ const updateNote = asyncHandler(async (req, res) => {
 const deleteNote = asyncHandler(async (req, res) => {
   const note = await Note.findById(req.params.id);
 
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
   if (note) {
+    await note.remove;
+    res.json({ message: "Note removed" });
   } else {
     res.status(404);
     throw new Error("Note not found");
